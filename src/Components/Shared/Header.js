92e0ef36d77b090266d4ea9f0e94c495f374e5b8
Pabitra9 <70833101+Pabitra9@users.noreply.github.com>
2024-01-31 12:09:47 +0530

    21th  commit

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -41,7 +41,7 @@ const Header = ({ isSidebarOpen, setSidebarOpen }) => {
   useEffect(()=>{
     if (!localStorage.getItem('userRole')) {
       console.log('hauchi');
-      alert('What are you doing without login')
+      // alert('What are you doing without login')
       navigate('/')
     }
   },[])
